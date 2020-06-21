b772e419fbad63a21ed65dcce9811fa258f06a7b
Kamuzaka <64137166+Kamuzaka@users.noreply.github.com>
2020-06-21 03:41:44 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+	return [
+		'database' => [
+			'user' => 'root',
+			'password' => '1234',
+			'host' => 'localhost',
+			'database' => 'hackaton'
+		],
+
+		'mail' => [
+			'username' => 'netpandemii.rf@yandex.ru',
+			'password' => 'sXHRSoQ8',
+			'host' => 'ssl://smtp.yandex.ru',
+			'port' => 465
+		]
+	];
